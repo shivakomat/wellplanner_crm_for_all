38901ff4544128e25bc813b65dd2998967d376a6
Shiva Komat <shivakomatreddyshops@gmail.com>
2025-08-10 19:22:37 -0700

    fix: add CASCADE and IF EXISTS to table drop statements in SQL evolution script

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -1,17 +1,19 @@
 # --- !Downs
-DROP TABLE "play_evolutions";
-DROP TABLE "teams";
-DROP TABLE "vendor_manage_categories";
-DROP TABLE "vendor_categories";
-DROP TABLE "vendor_manage";
-DROP TABLE "vendor_contacts_lists";
-DROP TABLE "vendor_buckets";
-DROP TABLE "budget_breakdowns";
-DROP TABLE "vendor_contacts";
-DROP TABLE "task_comments";
-DROP TABLE "task_items";
-DROP TABLE "tasks";
-DROP TABLE "projects";
-DROP TABLE "clients";
-DROP TABLE "users";
-DROP TABLE "businesses";
+DROP TABLE IF EXISTS "play_evolutions" CASCADE;
+DROP TABLE IF EXISTS "teams" CASCADE;
+DROP TABLE IF EXISTS "vendor_manage_categories" CASCADE;
+DROP TABLE IF EXISTS "vendor_categories" CASCADE;
+DROP TABLE IF EXISTS "vendor_manage" CASCADE;
+DROP TABLE IF EXISTS "vendor_contacts_lists" CASCADE;
+DROP TABLE IF EXISTS "vendor_buckets" CASCADE;
+DROP TABLE IF EXISTS "budget_breakdowns" CASCADE;
+DROP TABLE IF EXISTS "vendor_contacts" CASCADE;
+DROP TABLE IF EXISTS "task_comments" CASCADE;
+-- Remove FK constraint that links task_items to tasks before dropping tables
+ALTER TABLE "task_items" DROP CONSTRAINT IF EXISTS CONSTRAINT_4D CASCADE;
+DROP TABLE IF EXISTS "task_items" CASCADE;
+DROP TABLE IF EXISTS "tasks" CASCADE;
+DROP TABLE IF EXISTS "projects" CASCADE;
+DROP TABLE IF EXISTS "clients" CASCADE;
+DROP TABLE IF EXISTS "users" CASCADE;
+DROP TABLE IF EXISTS "businesses" CASCADE;
